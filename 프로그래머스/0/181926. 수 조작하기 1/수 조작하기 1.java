43c6e3a08aec13fafaaa66acc9a7c3f52f09f27d
ehouse16 <61807816+ehouse16@users.noreply.github.com>
2024-10-13 14:49:58 +0900

    [level 0] Title: 수 조작하기 1, Time: 6.01 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = 0;
+
+        for(char c : control.toCharArray())
+        {
+            switch(c){
+                case 'w':
+                    n++;
+                    break;
+                case 's':
+                    n--;
+                    break;
+                case 'd':
+                    n+=10;
+                    break;
+                case 'a':
+                    n-=10;
+                    break;
+            }
+        }
+        answer = n;
+        return answer;
+    }
+}
