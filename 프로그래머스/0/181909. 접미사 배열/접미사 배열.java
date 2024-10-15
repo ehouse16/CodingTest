57077a93477667f02f8382f549834a189f79605b
ehouse16 <61807816+ehouse16@users.noreply.github.com>
2024-10-15 17:55:07 +0900

    [level 0] Title: 접미사 배열, Time: 21.81 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -22,5 +22,16 @@ class Solution {
             answer[size++] = temp;
         }
         return answer;
+        
+        /*
+        String[] answer = {};
+        answer = new String[my_string.length()];
+
+        for(int i = 0; i<my_string.length(); i++){
+            answer[i] = my_string.substring(i,my_string.length());
+        }
+        Arrays.sort(answer);
+
+        return answer;*/
     }
 }
