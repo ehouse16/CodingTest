129f5c757f451d8992ee3ca7de6b27876227b63b
ehouse16 <61807816+ehouse16@users.noreply.github.com>
2024-10-15 17:54:20 +0900

    [level 0] Title: 접미사 배열, Time: 20.30 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String my_string) {
+       String[] answer = {};
+        answer = new String[my_string.length()];
+
+        String tmp = "";
+
+        ArrayList<String> arr = new ArrayList<>();
+
+        for (int i = 0; i < my_string.length(); i++) {
+            tmp = "";
+            for (int j = i; j < my_string.length(); j++) {
+                tmp+=my_string.charAt(j);
+            }
+            arr.add(tmp);
+        }
+        Collections.sort(arr);
+
+        int size=0;
+        for(String temp : arr){
+            answer[size++] = temp;
+        }
+        return answer;
+    }
+}
