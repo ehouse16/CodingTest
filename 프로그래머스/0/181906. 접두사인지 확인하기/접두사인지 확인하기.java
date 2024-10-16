41030fa4bf0a7e9d87911d0a8862f3d6ee881a45
ehouse16 <61807816+ehouse16@users.noreply.github.com>
2024-10-16 14:51:15 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.07 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        int answer = 0;
+        String[] arr = new String[my_string.length()];
+
+        for (int i = 0; i < my_string.length(); i++) {
+            arr[i] = my_string.substring(0,i+1);
+        }
+
+        for(int j = 0; j<my_string.length(); j++){
+            if(arr[j].equals(is_prefix))
+                answer = 1;
+        }
+        return answer;
+    }
+}
