d6cc26273205055510ba427c4bd776d1f9b74dbe
ehouse16 <61807816+ehouse16@users.noreply.github.com>
2024-10-22 13:12:20 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.02 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,21 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+class  Solution {
+    public String solution(String myString) {
+        String answer = "";
+        answer = myString.toLowerCase();
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        String str = "AbCdEfG";
+        String str2 = "AAA";
+
+        Solution s = new Solution();
+        System.out.println(s.solution(str));
+        System.out.println(s.solution(str2));
+    }
+
+}
